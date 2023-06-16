c0a39c63fdc4dcd5df97dfa99edb3aab036694af
kaniadhea <kaniadhea7@gmail.com>
2023-06-16 20:08:05 +0700

    int push

--- StackArray_170/StackArray_170.cpp
+++ StackArray_170/StackArray_170.cpp
@@ -5,5 +5,21 @@ using namespace std;
 
 class StackArray {
 private: 
+	int stack_array[5];
+	int top;
 
+public:
+	//constructor
+	StackArray() {
+		top = -1;
+	}
+
+	int push() {
+		int element;
+		cin >> element;
+		if (top == 4) {//step1
+			cout << "Number of data exceeds the lisit." << endl;
+			return;
+		}
+	}
 };
